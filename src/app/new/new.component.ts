a4a6f32a6e091ef0c5aa3a2ef36a4707e691491c
LeAnhTuanProgrammer <lleeaannhhttuuaann@gmail.com>
2020-03-24 01:56:24 +0700

    1 file modified

--- src/app/new/new.component.ts
+++ src/app/new/new.component.ts
@@ -7,20 +7,15 @@ import { NewService } from '../new.service';
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
-
   selected: Product;
   products: Product[];
-
   constructor(
     private newService: NewService
   ) { 
-
   }
-
   ngOnInit(): void {
     this.getProducts();
   }
- 
   getProducts(){
    this.newService.getProducts().subscribe(data => {
      this.products = data;
